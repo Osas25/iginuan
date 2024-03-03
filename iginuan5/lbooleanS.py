3f24352ebdba6fa6227bfe99e5387dcb5e254b55
Osas25 <emmanuelosarodion8@gmail.com>
2024-03-03 22:57:48 +0000

    i was very new to tech

---
+++ iginuan5/lbooleanS.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+e = 3
+em = 17
+emm = 5
+emma = 4
+emman = 89
+emmanu = 5
+emmanuel = 20
+if e <= em:
+    print("i understand")
+else:
+    print('wrong answer')
+
+if emm >= emmanu != emma:
+    print("i understand")
+else:
+    print('wrong answer')
+if emman > emmanuel > em >= e:
+    print("i understand the concept of boolean statements")
+else:
+    print('wrong answer ive still got a long way to go')
